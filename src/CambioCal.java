8adb77c98738c75b4e398a35dd74d8500dc12645
Murilo <oxelot17898@gmail.com>
2025-08-25 19:58:57 -0300

    Prótotipo de software para câmbio de mmoedas finalizado

---
+++ src/CambioCal.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class CambioCal {
+    String moedaOrigem;
+    String moedaDestino;
+    double valorCotacao;
+    double resultado;
+
+    public void receberDados(){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Qual a moeda de origem?");
+        moedaOrigem = leitor.nextLine();
+
+        System.out.println("Qual a moeda de destino?");
+        moedaDestino = leitor.nextLine();
+
+        System.out.println("Qual o valor do câmbio de um " + moedaDestino + " para um " + moedaOrigem + "?");
+        valorCotacao = leitor.nextDouble();
+
+        calcularCambio();
+    }
+
+    public void calcularCambio(){
+        resultado = 1/valorCotacao;
+
+        mostrarResultado();
+    }
+
+    public void mostrarResultado(){
+
+        System.out.println("1 " + moedaOrigem + " = " + resultado + " " + moedaDestino);
+
+        System.out.println("*********************");
+    }
+}
